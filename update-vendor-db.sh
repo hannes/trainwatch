24115f2e3c1ed72530b15598dde46ef55a2c2a43
Hannes Muehleien <hannes@muehleisen.org>
2015-03-01 07:43:44 +0000

    script fix

--- update-vendor-db.sh
+++ update-vendor-db.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 curl "https://code.wireshark.org/review/gitweb?p=wireshark.git;a=blob_plain;f=manuf" | \
-grep "^[0-9A-F][0-9A-F]:[0-9A-F][0-9A-F]:[0-9A-F][0-9A-F]\t" | \
+grep "^[0-9A-F][0-9A-F]:[0-9A-F][0-9A-F]:[0-9A-F][0-9A-F][[:space:]]" | \
 sed 's/ *#.*//' > mvs.tsv 
